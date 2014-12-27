3a2587ae13410f15274187d6ebb8087ea6513034
noxan <noxan@byteweaver.org>
2014-12-27 22:00:14 +0100

    use accounting package to format currency values

--- pocket-pad.js
+++ pocket-pad.js
@@ -1,17 +1,16 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+  var expense = -318.61;
+  var income = 419.15;
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+  Template.overview.helpers({
+    formatBalance: function() {
+      return accounting.formatMoney(income + expense);
+    },
+    formatIncome: function() {
+      return accounting.formatMoney(income);
+    },
+    formatExpense: function() {
+      return accounting.formatMoney(expense);
     }
   });
 
