2501a9d2991d1cf67745691f6bbe18e7e6bcd8ce
noxan <noxan@byteweaver.org>
2014-12-28 02:30:42 +0100

    duplicate count of dummy transactions and change format

--- server/fixtures/transactions.js
+++ server/fixtures/transactions.js
@@ -4,10 +4,14 @@ Meteor.startup = function() {
     console.log('There are no transactions. Start to import dummy transactions for testing...');
 
     var categories = Category.find({}).fetch();
-    for(var i=0; i<10; i++) {
+    for(var i=0; i<20; i++) {
       var amount = Math.random() * 5000 * (Math.random() > 0.5 ? 1 : -1);
       var categoryId = categories[Math.floor(Math.random() * categories.length)]._id;
-      Transaction.insert({name: 'Transaction #'+i, category: categoryId, amount: amount});
+      Transaction.insert({
+        name: 'Transaction #'+i,
+        category: categoryId,
+        amount: amount
+      });
     }
 
     console.log('Imported dummy transactions.');
