ad50faa6a7c1a8b85027786ad135f598d8fa6ebe
noxan <noxan@byteweaver.org>
2014-12-28 01:26:42 +0100

    add fixture for some dummy transactions

---
+++ server/fixtures/transactions.js
@@ -0,0 +1,15 @@
+Meteor.startup = function() {
+  if(Transaction.find({}).count() === 0 &&
+     Category.find({}).count() > 0) {
+    console.log('There are no transactions. Start to import dummy transactions for testing...');
+
+    var categories = Category.find({}).fetch();
+    for(var i=0; i<10; i++) {
+      var amount = (Math.random() * 5000 * (Math.random() > 0.5 ? 1 : -1)).toFixed(2);
+      var categoryId = categories[Math.floor(Math.random() * categories.length)]._id;
+      Transaction.insert({name: 'Transaction #'+i, category: categoryId, amount: amount});
+    }
+
+    console.log('Imported dummy transactions.');
+  }
+}
